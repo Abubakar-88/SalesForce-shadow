758e5b7373c04715ba649e75838380e50cceddb1
Abubakar-88 <76661821+Abubakar-88@users.noreply.github.com>
2023-06-10 12:22:36 +0600

    Update LandingPage.java

--- src/test/java/pages/LandingPage.java
+++ src/test/java/pages/LandingPage.java
@@ -15,12 +15,21 @@ public class LandingPage extends Base{
 	
 	
 	public void hoverOnLogin() {
-		 WebElement login = (WebElement)js.executeScript("return document.querySelector('hgf-c360nav').shadowRoot.querySelector('li.utility-icons-items.login>hgf-c360login').shadowRoot.querySelector('span:nth-child(2)')");
-		 mousehover(login);		
+			try {
+			 WebElement login = (WebElement)js.executeScript("return document.querySelector('hgf-c360nav').shadowRoot.querySelector('li.utility-icons-items.login>hgf-c360login').shadowRoot.querySelector('span:nth-child(2)')");
+			 mousehover(login);	
+		}catch(org.openqa.selenium.ElementNotInteractableException ex) {
+			
+		}	
 	}
 	public void clickOnSalesforce() {
-		 WebElement salesforce = (WebElement)js.executeScript("return document.querySelector('hgf-c360nav').shadowRoot.querySelector('li[class=\"utility-icons-items login\"]>hgf-c360login').shadowRoot.querySelector('h4')");
-		 click(salesforce);		
+			try {
+			 WebElement salesforce = (WebElement)js.executeScript("return document.querySelector('hgf-c360nav').shadowRoot.querySelector('li[class=\"utility-icons-items login\"]>hgf-c360login').shadowRoot.querySelector('h4')");
+		 click(salesforce);	
+		}catch(org.openqa.selenium.ElementNotInteractableException ex) {
+			
+		}
+			
 	}
 	
 	
